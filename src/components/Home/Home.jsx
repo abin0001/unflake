dd82f83d85d0a6a120578577cfeee7e621f5339f
abin0001 <abinj2003@gamail.com>
2024-05-26 21:45:47 +0530

    Navbar Finished

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../Navigation"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import NavbarBrand from 'react-bootstrap/NavbarBrand'
+import Image from "react-bootstrap/Image"
+import NavLink from 'react-bootstrap/NavLink'
+import Nav from 'react-bootstrap/Nav'
+
+import { Button } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Logo from "../Assets/logo192.png"
+
+
+const Navigation = () => {
+  return (
+    <>
+    <Navbar bg='black'>
+      <Container xxl className='bg-white'>
+        <NavbarBrand href='#'>
+          <Image src={Logo} fluid roundedCircle width={50} height={50}></Image>
+        </NavbarBrand>
+        <Nav>
+          <NavLink href='#'>Home</NavLink>
+          <NavLink href='#about'>About</NavLink>
+          <NavLink href='#Career'>Careers</NavLink>
+          <NavLink href='#Contact'>Contact</NavLink>
+        </Nav>
+        
+        <Button>
+          SIGN IN
+        </Button>
+
+        
+
+
+      </Container>
+    </Navbar>
+    </>
+  )
+}
+
+export default Navigation
